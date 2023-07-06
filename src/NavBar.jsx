6868abc3ae908d63956c952424e019742dab114f
DirtyKeyboard <andrewxx000222@icloud.com>
2023-07-06 18:17:44 -0500

    a

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
             </span>
             {open ? (
                 <div
-                    className={`h-screen w-screen fixed inset-0 bg-[#0000007e] ${tlate} backdrop-blur-sm`}
+                    className={`h-screen w-screen fixed bg-[#0000007e] ${tlate} backdrop-blur-sm`}
                 />
             ) : null}
             <svg
